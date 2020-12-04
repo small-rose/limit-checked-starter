9f863a86591f4ea6b33e9c366a6f57e769b74f4a
small-rose <small-rose@qq.com>
2020-12-05 00:03:01 +0800

    add demo and update readme

--- limit-checked-demo/src/main/java/cn/xiaocai/limit/checked/demo/controller/DemoController.java
+++ limit-checked-demo/src/main/java/cn/xiaocai/limit/checked/demo/controller/DemoController.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     /**
      * use @LimitChecked
-     * @return
+     * 访问该接口限制：10秒钟内只能访问2次
+     * @return String
      */
     @RequestMapping("/test")
     @LimitChecked(name = "test", second = 10, totalCount = 2)
@@ -22,9 +23,22 @@ public class DemoController {
     }
 
     /**
+     * use @LimitChecked just limit on call success
+     * when exception happened ,the limit numbers not count
+     * 访问该接口限制：10秒内，限制成功访问2次
+     * @return String
+     */
+    @RequestMapping("/onSuccess")
+    @LimitChecked(name = "success", second = 10, totalCount = 2, onlySuccessCount = true)
+    public String success(){
+        return "ok";
+    }
+
+    /**
      * when no params
      * use global application.properties
-     * @return
+     * 该接口的限制参数都没有配置，则使用 application.properties 全局配置
+     * @return String
      */
     @RequestMapping("/test2")
     @LimitChecked(name = "global")
