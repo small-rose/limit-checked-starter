3e59d66751051ff6c613247aa6cf3e710d5de607
small-rose <small-rose@qq.com>
2021-04-13 19:23:42 +0800

    add demo

--- limit-checked-demo/src/main/java/cn/xiaocai/limit/checked/demo/LimitCheckedDemoApplication.java
+++ limit-checked-demo/src/main/java/cn/xiaocai/limit/checked/demo/LimitCheckedDemoApplication.java
@@ -1,12 +1,16 @@
 package cn.xiaocai.limit.checked.demo;
 
+import cn.xiaocai.limit.core.v3.annotation.LimitCheckedScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Xiaocai.Zhang
  */
+
 @SpringBootApplication
+// @EnableLimitChecked // use style v2
+@LimitCheckedScans(basePackage = "cn.xiaocai.limit.checked.demo") // use style v3
 public class LimitCheckedDemoApplication {
 
     public static void main(String[] args) {
