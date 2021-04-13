bab7ffe9228942f8e6127c21b74f5cda2b181825
small-rose <small-rose@qq.com>
2021-04-13 19:24:21 +0800

    add demo

--- limit-checked-spring-boot-starter/src/main/java/cn/xiaocai/limit/checked/core/auto/LimitCheckedAutoConfiguration.java
+++ limit-checked-spring-boot-starter/src/main/java/cn/xiaocai/limit/checked/core/auto/LimitCheckedAutoConfiguration.java
@@ -1,14 +1,12 @@
 package cn.xiaocai.limit.checked.core.auto;
 
 
-import cn.xiaocai.limit.checked.core.annotation.EnableLimitChecked;
-import cn.xiaocai.limit.checked.core.annotation.LimitChecked;
 import cn.xiaocai.limit.checked.core.aop.LimitCheckedAspect;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.Resource;
